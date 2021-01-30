ecf005461ee9fe3df8331daec17fdcb01120c0d4
jojo39381 <josephycyeh@gmail.com>
2021-01-30 01:22:47 -0800

    a

---
+++ Example/Pods/Target Support Files/Pods-Puzzl_Example/Pods-Puzzl_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Puzzl_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Puzzl_Example
+@end
---
+++ Example/Pods/Target Support Files/Puzzl/Puzzl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Puzzl : NSObject
+@end
+@implementation PodsDummy_Puzzl
+@end
